787ec18c8bf20e79faea3fcd2b6a868bf3479377
ManideepNarla22 <manideepnarla321@gmail.com>
2026-09-23 12:33:25 +0530

    Upadte address

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -54,7 +54,7 @@ const Contact = () => {
                 <div>
                   <h3 className="font-serif text-xl text-stone-900 mb-2">Address</h3>
                   <p className="text-stone-600 leading-relaxed">
-                    4th Floor, 1-2-39/10, above Aakruti,20 meters from JNTU Metro Station,<br />
+                    4th Floor, 1-2-39/10, above Aakruti,200 meters from JNTU Metro Station,<br />
                     Nizampet Main Road, Hyderabad<br />
                     
                   </p>
@@ -147,7 +147,7 @@ const Contact = () => {
               </div>
               <h3 className="text-2xl font-serif text-stone-900 mb-4">Skin Atelier</h3>
               <p className="text-stone-600 mb-8 leading-relaxed text-lg">
-                4th Floor, 1-2-39/10, above Aakruti,20 meters from JNTU Metro Station,<br />
+                4th Floor, 1-2-39/10, above Aakruti,200 meters from JNTU Metro Station,<br />
                 Nizampet Main Road, Hyderabad<br />
                 
               </p>
